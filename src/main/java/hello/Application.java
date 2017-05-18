29b46f9a5b9411bdfc4bb12858029aa0e9fa0339
Nilson Junior <tenaz3.comp@gmail.com>
2017-05-18 19:52:43 +0200

    Initial Spring Structure

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by tenaz3 on 18.05.17.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
